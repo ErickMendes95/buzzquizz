234cb92a89e857314d783529e6e1aea8e19984a3
geisniwander <geisniwander@gmail.com>
2022-11-03 15:28:42 -0300

    "inserting question fields by javascript"

--- script.js
+++ script.js
@@ -102,3 +102,89 @@ function changeQuizzResults(quizz, levelSelected) {
     quizzResults.innerHTML = quizzResultsHTML[levelSelected];
 }
 /* FIM JAVASCRIPT DESENVOLVIDO PARA A TELA 2 - ÉRICO */
+
+
+
+
+/* JAVASCRIPT - CADASTRO DE QUIZZ - GPS*/
+let quizzTitle = "";
+let quizzImage = "";
+let quizzNumberQuestions = 0;
+let quizzNumberLevels = 0;
+
+function saveInformations(){
+    let containerInformations = document.querySelector('.quizzInformation')
+    quizzTitle = document.querySelector(".quizzTitle").value;
+    quizzNumberQuestions = document.querySelector(".quizzNumberQuestions").value;
+    quizzNumberLevels = document.querySelector(".quizzNumberLevels").value;
+    try{
+        quizzImage = new URL (document.querySelector(".quizzImage").value);
+    }catch(err){
+        quizzImage=null;
+    }
+
+    if(quizzTitle.length<20 || quizzTitle.length>65 || !quizzImage || quizzNumberQuestions<3 || quizzNumberLevels<2){
+        alert("Informações erradas")
+    }
+    else{
+        console.log(containerInformations.classList)
+        containerInformations.classList.add("hidden");
+        insertQuestionFields();
+    }
+    
+}
+
+function insertQuestionFields(){
+    let containerQuestionFields = document.querySelector(".quizzQuestionsForm");
+    console.log(containerQuestionFields.classList)
+    containerQuestionFields.classList.remove("hidden");
+    for(let i=1; i<=quizzNumberQuestions; i++){
+        containerQuestionFields.innerHTML += 
+        `<div class="forms question${i}">
+        <form action="formInformation">
+            <h1 class="subtitleForm">Pergunta ${i}</h1>
+            <input type="text" class="textQuestion${i}" placeholder="Texto da pergunta">
+            <input type="text" class="backgroundQuestion${i}" placeholder="Cor de fundo da pergunta">
+            <h1 class="subtitleForm">Resposta Correta</h1>
+            <input type="text" class="correctAnswerQuestion${i}" placeholder="Resposta correta">
+            <input type="url" class="imageCorrectAnswerQuestion${i}" placeholder="URL da imagem">
+            <h1 class="subtitleForm">Respostas incorretas</h1>
+            <input type="text" class="incorrectAnswerQuestion${i}"placeholder="Resposta incorreta 1">
+            <input type="url" class="imageIncorrectAnswerQuestion${i}" placeholder="URL da imagem 1">
+            <br>                
+            <input type="text" class="incorrectAnswerQuestion${i}" placeholder="Resposta incorreta 2">
+            <input type="url" class="imageIncorrectAnswerQuestion${i}" placeholder="URL da imagem 2">
+            <br>                
+            <input type="text" class="incorrectAnswerQuestion${i}" placeholder="Resposta incorreta 3">
+            <input type="url" class="imageIncorrectAnswerQuestion${i}" placeholder="URL da imagem 3">
+        </form>
+    </div>`
+    }
+    containerQuestionFields.innerHTML+= `
+    <div class="question2">
+        <h1 class="subtitleForm">Pergunta 2</h1>
+        <ion-icon name="create-outline"></ion-icon>
+    </div>
+    <div class="question3">
+        <h1 class="subtitleForm">Pergunta 3</h1>
+        <ion-icon name="create-outline"></ion-icon>
+    </div>
+    <button class="buttonForm" onclick="saveQuestions()">Prosseguir pra criar níveis</button>
+</section>`
+}
+
+function saveQuestions(){
+
+}
+
+function saveLevels(){
+
+}
+
+function accessQuizz(){
+
+}
+
+function home(){
+
+}
